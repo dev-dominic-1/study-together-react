10c12255ace5151c5deec77df86a3d57989381d9
Dominic Hiland <dominic.dev.email@gmail.com>
2024-04-07 22:38:10 -0500

    Centered column on FriendsTabContent component:

--- src/components/routes/home/friends/FriendsTabContent.tsx
+++ src/components/routes/home/friends/FriendsTabContent.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Post } from "../../../../models/Post";
 import PostCard from "../../../post-card/PostCard";
+import styles from  "./FriendsTabContent.module.scss"
 
 let testPost = new Post('Albert Einstein', '16 Hours Ago', 'Sample Content')
 
 export default function FriendsTabContent() {
 
-  const posts = [testPost, testPost, testPost]
+  const posts: Post[] = [testPost, testPost, testPost]
 
   return (
-    <>
+    <div className={styles.wrapper}>
       {posts.map((p, i) => (
         <PostCard key={`post-card-${i}`} post={p} />
       ))}
-    </>
+    </div>
   )
 }
