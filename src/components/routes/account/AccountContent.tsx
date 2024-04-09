a367eb7879c02b8aa2abb906dc27f0e345738f81
Dominic Hiland <dominic.dev.email@gmail.com>
2024-04-08 22:03:42 -0500

    Started on AccountContent component

--- src/components/routes/account/AccountContent.tsx
+++ src/components/routes/account/AccountContent.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
+import styles from "./AccountContent.module.scss";
+import bannerImage from "assets/Images/Desktop/Profile/2x/Profile-Cover-Photo-Desktop2x.jpg";
+import profileImage from "assets/Images/Desktop/Profile/2x/Profile-Profile-Picture-Desktop2x.jpg";
 
 export default function AccountContent() {
   return (
-    <div className="app__background-config-2">
-      <Typography variant={"h3"}>
-        ACCOUNT COMPONENT
-      </Typography>
+    <div className={`${styles.wrapper} app__background-config-2`}>
+      <div className={styles.bannerContainer}>
+        <img
+          src={bannerImage}
+          alt="banner"
+        />
+      </div>
+      <div className={styles.bioContentContainer}>
+        <div className={styles.profileImageWrapper}>
+          <img
+            src={profileImage}
+            alt="profile"
+          />
+        </div>
+      </div>
+      <Typography variant={"h3"}>ACCOUNT COMPONENT</Typography>
     </div>
-  )
+  );
 }
